f3f8c8fff4424b82c1e6af98626706248971aeb6
Anja <79693733+alabak48@users.noreply.github.com>
2022-12-06 18:51:55 +0100

    Update basic.e2e.js

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -3,6 +3,17 @@ describe("Shoutem web", async () => {
         it("Check if Shoutem Pricing page is loading", async () => {
             await browser.url("https://shoutem.com/pricing/");
 
-            await browser.pause(5000);
+           const heading = await $("h1");
+           await expect(heading).toHaveText("There are more ways to create apps with Shoutem");
+        });
+
+        it("Check if template is loaded", async () => {
+            await browser.url("https://shoutem.com/apps/social-network/");
+
+           const button = await $('a=Build now');
+           await button.click();
+           
+           const heading = await $(".page-title");
+           await expect(heading).toHaveText("Choose an app template");
         });
 });
